4900e0d1cccb3f9993640d4db84a76c1b9cb69ce
Mohamed Sobh <eng.m_sobhe@yahoo.com>
2022-01-02 11:32:14 +0000

    add Candidate model and migrate

--- candidates_api/admin.py
+++ candidates_api/admin.py
@@ -1,3 +1,6 @@
+from candidates_api.models import Candidate
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Candidate)
--- candidates_api/models.py
+++ candidates_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=60)
+    candidate_ref = models.CharField(max_length=8)
+
+    def __str__(self):
+        return self.name
