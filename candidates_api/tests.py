592160d404916de7a4adf666c5af5c0367303959
Mohamed Sobh <eng.m_sobhe@yahoo.com>
2022-01-02 14:56:17 +0000

    Add Testing plan to be executed

--- candidates_api/tests.py
+++ candidates_api/tests.py
@@ -1,3 +1,59 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+#####################
+# 1- Testing Models #
+################################
+# 1-a- Testing Candidate Model #
+################################
+# candidate_ref must be a unique sequence of 8 digits and alphabets.
+
+
+############################
+# 1-b- Testing Score Model #
+############################
+# score must be float between 0 - 100 inclusive.
+
+
+# score must be stored in a list.
+
+
+#########################
+# 2- Testing Endpoints  #
+##################################
+# 2-a- Testing /create-candidate #
+##################################
+# Response with 201 code
+
+
+# Handle invalid values (candidate_ref must be a unique sequence of 8 digits and alphabets) respond with status code(400)Bad request
+
+
+# Handle duplicate values (check if candidate_ref already exist) respond with status code (409)conflict
+
+
+###############################
+# 2-b- Testing /create-score  #
+###############################
+# Respond with status code (201)
+
+
+# It should write a single score to the dB.
+
+
+# Score should be float (0 - 100) inclusive.
+
+
+# Score must be pushed and stored into a list.
+
+
+################################################
+# 2-c- Testing /get-candidate/{candidate_ref}  #
+################################################
+# Respond with status code (200).
+
+
+# Response should be a dict. formatted as : {'candidate_ref':'8u9i7y6t', 'name':'Test', 'score':[34,56,....]}.
+
+
+# If (candidate_ref) doesn't exist respond with a msg 'Candidate not registered' and a status code(404).
